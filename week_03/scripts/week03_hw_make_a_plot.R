83e46f1230481377d3642ab8664dc981f37cefe9
jakereichard <jakereic@hawaii.edu>
2025-09-13 15:13:29 -1000

    adding in markdown info

--- week_03/scripts/week03_hw_make_a_plot.R
+++ week_03/scripts/week03_hw_make_a_plot.R
@@ -19,22 +19,27 @@ ggplot(data = penguins,
                     fill=species))+ #assigning data to axes
     geom_violin(alpha = 0.2)+ #making a violin plot that is transparent 
     geom_boxplot( alpha= 0.5, width=0.1)+ #box plot within violin to show distribution
+    
     theme_bw()+ #makes the background white
     scale_fill_viridis_d()+ #makes the fill colorblind friendly
     scale_color_viridis_d()+ #makes the outline color colorblind friendly
+    
     coord_flip()+ #flip axes to make it easier to read
-  scale_y_continuous(breaks = seq(20,60,10))+
-  theme(legend.position = "none", #removed the legend
+    
+    scale_y_continuous(breaks = seq(20,60,10))+ #create the breaks for y axis
+  
+    theme(legend.position = "none", #removed the legend
         plot.title = element_text(size = 18))+ #make the title bigger 
   
   #Editing labels for the figure
   labs(x = "Species",
-       caption = "Data Source:Palmer Penguins Package",
+       caption = "Data Source: Palmer Penguins Package", #identify data source
        y = "Bill Length (mm)",
-       title = expression(bold("Bill length across three penguis species")),
+       title = expression(bold("Bill length across three penguis species")), #bold the text 
        subtitle = "Across three island: Biscoe, Dream, Torgersen")
 
 ## exporting data as PNG file ##
-ggsave(here("week_03","outputs","week03_hw_bill_L_by_species.png",
-            width=7, height=5))
+
+ggsave(here("week_03","outputs", "week03_hw_bill_L_by_species.png"), #telling it where to save
+            width = 7, height = 5) #size in inches
   
